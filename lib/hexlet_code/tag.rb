f2f55099ba897103aabdc6a37569eaa73d317821
Aleksandr Mihailov <mikhaylov.av22@gmail.com>
2022-07-11 14:03:58 +0300

    refactoring

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,16 +4,16 @@
 module HexletCode
   # class Tag
   class Tag
-      def self.build(tag, *attributes, &block)
-        return %(<#{tag}>) if attributes.empty? && block.nil?
-        return %(<#{tag}>#{yield}</#{tag}>) if attributes.empty? && block.nil? == false
+    def self.build(tag, *attributes, &block)
+      return %(<#{tag}>) if attributes.empty? && block.nil?
+      return %(<#{tag}>#{yield}</#{tag}>) if attributes.empty? && block.nil? == false
 
-        arr = []
-        attrs, * = attributes
+      arr = []
+      attrs, * = attributes
 
-        attrs.each_pair { |key, value| arr << %(#{key}="#{value}") }
+      attrs.each_pair { |key, value| arr << %(#{key}="#{value}") }
 
-        block ? %(<#{tag} #{arr.join(' ')}>#{yield}</#{tag}>) : %(<#{tag} #{arr.join(' ')}>)
+      block ? %(<#{tag} #{arr.join(' ')}>#{yield}</#{tag}>) : %(<#{tag} #{arr.join(' ')}>)
     end
   end
 end
